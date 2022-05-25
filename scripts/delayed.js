8d18c47806bc71881ce9ccaf30ae7a552724705d
David Nuescheler <david@nuescheler.ch>
2022-05-25 12:45:06 -0600

    chore: LH update

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,16 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function loadScript(url, callback, type) {
+  const $head = document.querySelector('head');
+  const $script = document.createElement('script');
+  $script.src = url;
+  if (type) {
+    $script.setAttribute('type', type);
+  }
+  $head.append($script);
+  $script.onload = callback;
+  return $script;
+}
+
+loadScript('https://alcdn.msauth.net/browser/2.24.0/js/msal-browser.min.js');
