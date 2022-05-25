eb20314ebb72119d17f21e904000f7bca366dadd
David Nuescheler <david@nuescheler.ch>
2022-05-25 15:50:33 -0600

    chore: adjust async log in

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -8,8 +8,6 @@ import {
   decorateBlock,
 } from '../../scripts/scripts.js';
 
-import { signIn, signOut, getCurrentAccount } from '../../scripts/auth.js';
-
 /**
  * collapses all open nav sections
  * @param {Element} sections The container element
@@ -49,41 +47,6 @@ export default async function decorate(block) {
       const section = nav.children[j];
       if (section) section.classList.add(`nav-${e}`);
     });
-
-    // ----< tripod's auth poc >-------------------
-    // hack: get sign-in button
-    const authNavi = nav.children[0].children[1];
-    while (authNavi.firstChild) {
-      authNavi.firstChild.remove();
-    }
-    const account = await getCurrentAccount();
-    if (account) {
-      authNavi.appendChild(document.createTextNode('Welcome '));
-      const btnProfile = document.createElement('a');
-      authNavi.appendChild(btnProfile);
-      btnProfile.innerText = account.name;
-      btnProfile.href = '/profile.html';
-      authNavi.appendChild(document.createTextNode(' | '));
-
-      const btnSignOut = document.createElement('a');
-      authNavi.appendChild(btnSignOut);
-      btnSignOut.innerText = 'Sign out';
-      btnSignOut.style.cursor = 'pointer';
-      btnSignOut.onclick = signOut;
-    } else {
-      const btnSignIn = document.createElement('a');
-      authNavi.appendChild(btnSignIn);
-      btnSignIn.innerText = 'Sign in';
-      btnSignIn.onclick = signIn;
-      btnSignIn.style.cursor = 'pointer';
-      authNavi.appendChild(document.createTextNode(' or '));
-      const btnRegister = document.createElement('a');
-      authNavi.appendChild(btnRegister);
-      btnRegister.href = '/content/eecol/ca/en/register';
-      btnRegister.innerText = 'Register';
-      authNavi.appendChild(document.createTextNode(' CAD'));
-    }
-    // ----< eof tripod's auth poc >-------------------
 
     const navSections = [...nav.children][2];
     if (navSections) {
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -17,4 +17,43 @@ function loadScript(url, callback, type) {
   return $script;
 }
 
-loadScript('https://alcdn.msauth.net/browser/2.24.0/js/msal-browser.min.js');
+loadScript('https://alcdn.msauth.net/browser/2.24.0/js/msal-browser.min.js', async () => {
+  const { signIn, signOut, getCurrentAccount } = await import('./auth.js');
+
+  // ----< tripod's auth poc >-------------------
+  // hack: get sign-in button
+  const nav = document.querySelector('nav');
+  console.log(nav);
+  const authNavi = nav.children[1].children[1];
+  while (authNavi.firstChild) {
+    authNavi.firstChild.remove();
+  }
+  const account = await getCurrentAccount();
+  if (account) {
+    authNavi.appendChild(document.createTextNode('Welcome '));
+    const btnProfile = document.createElement('a');
+    authNavi.appendChild(btnProfile);
+    btnProfile.innerText = account.name;
+    btnProfile.href = '/profile.html';
+    authNavi.appendChild(document.createTextNode(' | '));
+
+    const btnSignOut = document.createElement('a');
+    authNavi.appendChild(btnSignOut);
+    btnSignOut.innerText = 'Sign out';
+    btnSignOut.style.cursor = 'pointer';
+    btnSignOut.onclick = signOut;
+  } else {
+    const btnSignIn = document.createElement('a');
+    authNavi.appendChild(btnSignIn);
+    btnSignIn.innerText = 'Sign in';
+    btnSignIn.onclick = signIn;
+    btnSignIn.style.cursor = 'pointer';
+    authNavi.appendChild(document.createTextNode(' or '));
+    const btnRegister = document.createElement('a');
+    authNavi.appendChild(btnRegister);
+    btnRegister.href = '/content/eecol/ca/en/register';
+    btnRegister.innerText = 'Register';
+    authNavi.appendChild(document.createTextNode(' CAD'));
+  }
+  // ----< eof tripod's auth poc >-------------------
+});
