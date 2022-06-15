39ae75de2778f8c8c90599c22aa203ef1aa5db66
David Nuescheler <david@nuescheler.ch>
2022-06-15 08:53:25 -0700

    chore: simple blocks

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,5 +1,4 @@
 export default function decorate(block) {
-  block.querySelectorAll(':scope > div > div').forEach((cell) => {
-    if (cell.querySelector('picture') && cell.querySelector('h2')) cell.classList.add('columns-overlay');
-  });
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
 }
