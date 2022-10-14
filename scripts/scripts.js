0b73d2061d6652357bb1a10d93a42f4874e97d49
Max Edell <maxed@adobe.com>
2022-10-14 15:29:14 -0700

    self origin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,11 +22,9 @@ import {
   decorateSections,
 } from './helix-web-library.esm.js';
 
-const UPSTREAM_PROD = 'https://main--eecol--hlxsites.helix3.dev';
-const UPSTREAM_DEV = 'http://localhost:3000';
-const dev = window.location.origin === UPSTREAM_DEV
-  || new URL(window.location.href).searchParams.get('dev') === 'true';
-const upstreamURL = dev ? UPSTREAM_DEV : UPSTREAM_PROD;
+const url = new URL(window.location.href);
+const upstreamURL = url.searchParams.get('upstream')
+ || window.location.origin;
 
 const loggedIn = !!sessionStorage.getItem('account');
 const loginRedirect = sessionStorage.getItem('loginRedirect') === 'true';
